c4d7a7777f83734fe5e4e798671ee5569834b86c
Parasite-231 <muktadir@iut-dhaka.edu>
2022-09-08 15:01:17 +0600

    test commit for user login page

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import UserLoginPage from "./pages/user pages/UserLoginPage";
 function App() {
   return (
     <>
+      <h1>User Login Page</h1>
       <UserLoginPage />
       {/* <UserRegistrationPage /> */}
     </>
