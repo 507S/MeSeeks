adf7b8374d0b4f5bb296736b5643a67dfb6889e7
Parasite-231 <muktadir@iut-dhaka.edu>
2022-12-29 19:12:51 +0600

    ban worker

--- src/components/admin components/admin dashboard additional components/worker information/worker ban section/AdminDashboardBanWorkerPageLayout.js
+++ src/components/admin components/admin dashboard additional components/worker information/worker ban section/AdminDashboardBanWorkerPageLayout.js
@@ -3,109 +3,24 @@ import React from "react";
 // import "../../../styles/global/admin global/AdminDashboardStyles.css";
 // import "../../../styles/global/admin global/bootstrap.min.css";
 // import "../../../styles/global/admin global/bootstrap.min.css.map";
-import axios from "axios";
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import classes from "../../../../../styles/admin styles/AdminDashboardSideBarNavListSize.module.css";
 import "../../../../../styles/global/admin global/AdminDashboardCards.css";
 import "../../../../../styles/global/admin global/AdminDashboardStyles.css";
 import "../../../../../styles/global/admin global/bootstrap.min.css";
+import AdminDashboardSideBarNavListPartials from "../../../admin dashboard components/admin sideBarNavList partials/AdminDashboardSideBarNavListPartials";
 import AdminDashboardAuthenticationButton from "../../../admin dashboard components/AdminDashboardAuthenticationButton";
 import AdminDashboardHeaderContent from "../../../admin dashboard components/AdminDashboardHeaderContent";
 import AdminDashboardNavBarSearchForm from "../../../admin dashboard components/AdminDashboardNavBarSearchForm";
 import AdminDashboardNavBarSearchTextInput from "../../../admin dashboard components/AdminDashboardNavBarSearchTextInput";
 import AdminDashboardNavBarTogglerButton from "../../../admin dashboard components/AdminDashboardNavBarTogglerButton";
 import AdminDashboardSideBarHeaderSection from "../../../admin dashboard components/AdminDashboardSideBarHeaderSection";
-import { default as AdminDashboardSideBarNavListEndSection, default as AdminDashboardSideBarNavListPartials } from "../../../admin dashboard components/AdminDashboardSideBarNavListEndSection";
+import AdminDashboardSideBarNavListEndSection from "../../../admin dashboard components/AdminDashboardSideBarNavListEndSection";
 import AdminDashboardActionButton from "../../admin dashboard actionForm components/AdminDashboardActionButton";
 import AdminDashboardActionForm from "../../admin dashboard actionForm components/AdminDashboardActionForm";
 import TextAreaInput from "../../admin dashboard actionForm components/TextAreaInput";
 import TextInput from "../../admin dashboard actionForm components/TextInput";
-// import AdminDashboardSideBarNavListEndSection from "../../admin dashboard components/AdminDashboardSideBarNavListEndSection";
-export default function AdminDashboardBanWorkerPageLayout() {
-  
-  const navigate = useNavigate();
-
-  // const [name, setName] = useState("");
-  // const [description, setDescription] = useState("");
-  // const [error, setError] = useState(false);
-  // const [loading, setLoading] = useState(false);
-  
-
-  // const submitHandler = async (e) => {
-  //   e.preventDefault();
-  //     try {
-  //       const config = {
-  //         headers: {
-  //           "Content-type": "application/json",
-  //         },
-  //       };
-
-  //       setLoading(true);
-
-  //       const { data } = await axios.post(
-  //         "/api/services/addservice",
-  //         {
-  //           name,
-  //           description
-  //         },
-  //         config
-  //       )
-  //       .then(res => {
-  //         console.log(res);
-  //       });
-
-  //       console.log(data);
-  //       localStorage.setItem("userInfo", JSON.stringify(data));
-  //       setLoading(false);
-  //       window.location.href = "/";
-  //     } catch (error) {
-  //       setError(error.response.data.message);
-  //     }
-  //   }
-
-  const [inpval, setINP] = useState({
-    name: "",
-    description: "",
-})
-
-const setdata = (e) => {
-    const { name, value } = e.target;
-    console.log(name)
-    console.log(value)
-    setINP((preval) => {
-        return {
-            ...preval,
-            [name]: value
-        }
-    })
-}
-const addinpdata = async(e) =>{
-  e.preventDefault();
-
-   try{
-    console.log(inpval)
-    const res = await axios.post("http://localhost:8003/api/services/addservice", inpval);
-    // console.log(res)
-    // const data = await res.json();
-    console.log(res);
-  
-    if(res.status === 422){
-        alert("error");
-        console.log("error");
-    }
-    else{
-        alert("data added");
-        console.log("data added");
-        navigate("/admin-dashboard/active-service-category", { replace: true });
-    }
-   }
-   catch(error){
-    alert(error)
-   }
-  
-}
-return (
+export default function AdminDashboardWorkerBanPageLayout() {
+  return (
     <>
       <header className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
         <AdminDashboardSideBarHeaderSection adminSideBarHeaderText="MeSeeks" />
@@ -159,8 +74,7 @@ return (
               <div className="btn-toolbar mb-2 mb-md-0"></div>
             </div>
 
-            {/* //Form starts */}
-
+            {/* //Data Table Section starts */}
             <div class="card w-100 mt-5" style={{ height: "auto" }}>
               <div class=" card-body w-100" style={{ height: "auto" }}>
                 {/* <form method="POST" enctype="multipart/form-data"> */}
@@ -170,8 +84,8 @@ return (
                     labelName="Worker ID : "
                     type="text"
                     placeholder="Provide a Worker ID"
-                    value={inpval.name} 
-                    onChange={setdata} 
+                    // value={inpval.name} 
+                    // onChange={setdata} 
                   />
                   <TextAreaInput
                     name="description"
@@ -179,8 +93,8 @@ return (
                     labelName="Provide Ban Message :"
                     placeholder="Add a ban message..."
                     textAreaInputRow="4"
-                    value={inpval.description}
-                    onChange={setdata} 
+                    // value={inpval.description}
+                    // onChange={setdata} 
                     style={{ reSize: "none" }}
                   />
 
@@ -192,7 +106,7 @@ return (
                       adminActionButtonText="Add Service Category"
                       type="submit"
                       value="Submit" 
-                      onClick={addinpdata}
+                      // onClick={addinpdata}
                     />
                   </div>
                 </AdminDashboardActionForm>
